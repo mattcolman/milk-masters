341bef0f1128fac81c78b5eac1061f3204ac2e4c
Matt Colman <mcolman@atlassian.com>
2026-05-12 21:27:33 +1000

    update base path in vite.config.ts for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/m-masters/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/milk-masters/' : '/',
   plugins: [react(), tailwindcss()],
 });
